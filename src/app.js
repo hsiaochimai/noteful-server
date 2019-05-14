7282443701146a0d6f797068902b8e940d00bafa
Jennifer Hsiao <jchmai21@gmail.com>
2019-05-14 06:39:04 -0700

    added data to seeds and folder router and service

--- src/app.js
+++ src/app.js
@@ -4,9 +4,16 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-
+const winston = require('winston');
+const foldersRouter = require ('./folders/folders-router')
 const app = express()
-
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
@@ -14,10 +21,19 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
 
-app.get('/',(req,res)=>{
-  res.send('Hello, world!')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
 })
+
+app.use('/api/folders', foldersRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
